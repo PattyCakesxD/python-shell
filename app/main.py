4bc0278bc2df6a5e0b32570805c45d714a2105c8
PattyCakesxD <nguyenpatty42@gmail.com>
2026-01-29 20:44:45 -0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,17 +1,24 @@
 import sys
 
+VALID_FUNCTIONS = ["exit", "echo", "type"]
 
 def main():
     while True:
-        cmd = input("$ ")
+        userInput = input("$ ")
+        res = userInput.split()
+        cmd = res[0]
 
         if cmd == "exit":
             break
-        elif cmd[:5] == "echo ":
-            print(cmd[5:])
+        elif cmd == "echo":
+            print(" ".join(res[1:]))
+        elif cmd == "type":
+            if res[1] in VALID_FUNCTIONS:
+                print(f"{res[1]} is a shell builtin")
+            else:
+                print(f"{res[1]}: not found")
         else:
             print(f"{cmd}: command not found")
-
         
 
 if __name__ == "__main__":
